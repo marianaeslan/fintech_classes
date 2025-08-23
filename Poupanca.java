7ec8bd934d8fba767446363663fec0740d5ba7ee
marianaeslan <mariana.eslan@gmail.com>
2025-08-23 17:38:06 -0300

    feat: implemented Poupanca method

--- Poupanca.java
+++ Poupanca.java
@@ -1,2 +1,34 @@
 public class Poupanca {
+    private double valor;
+    private double saldoPoupanca;
+    private String descricao;
+    public Poupanca(double valor, double saldo, String descricao) {
+        this.valor = valor;
+        this.saldoPoupanca = saldoPoupanca;
+        this.descricao = descricao;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    public double getSaldo() {
+        return saldoPoupanca;
+    }
+    public void setSaldo(double saldo) {
+        this.saldoPoupanca = saldo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public void addSaldo(double saldo, double valor) {
+        saldoPoupanca += valor;
+    }
+    public void subSaldo(double saldo, double valor) {
+        saldoPoupanca -= valor;
+    }
 }
